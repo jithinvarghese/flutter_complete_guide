4f77d57a4cee22b78b0215c185027f6e5dca55b9
Jithin varghese <jithinvpynadath@gmail.com>
2020-08-25 20:50:47 +1200

    widegets

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionindex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionindex = questionindex + 1;
+    });
+    print(questionindex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "what's your favorite color?",
+      "whats's your favorite animal?"
+    ];
     return MaterialApp(
-      home: Text("Hello !"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionindex)),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
